1b02188fec0ac4b87b3f28434de8594e2accc7b9
RbMuhib <raghibbmuhib@gmail.com>
2019-09-26 07:05:23 -0400

    extract

--- app/components/person-information.js
+++ app/components/person-information.js
@@ -1,4 +1,13 @@
 import Component from '@ember/component';
+import {inject as service}  from '@ember/service'
 
 export default Component.extend({
+    init() {
+        this._super(...arguments);
+        let head=this.store.findAll('person');
+        console.log(head);
+        store: service(),
+        
+
+    }
 });
