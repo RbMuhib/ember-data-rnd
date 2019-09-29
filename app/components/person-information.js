f1e6edca2abae46450cb4e2392633808d7e64bcd
RbMuhib <raghibbmuhib@gmail.com>
2019-09-29 01:11:17 -0400

    table data added

--- app/components/person-information.js
+++ app/components/person-information.js
@@ -1,13 +1,23 @@
 import Component from '@ember/component';
-import {inject as service}  from '@ember/service'
+import { inject as service } from '@ember/service'
 
 export default Component.extend({
+    store: service(),
+    data:[],
+
     init() {
         this._super(...arguments);
-        let head=this.store.findAll('person');
-        console.log(head);
-        store: service(),
-        
+        let head = this.get('store').peekAll('person');
+        for(let i=0; i<head.length ; i++){
+            let rowInfo={
+                id: head._objects[i]._internalModel.__recordData._attributes.id,
+                fname: head._objects[i]._internalModel.__recordData._attributes.firstname,
+                lname: head._objects[i]._internalModel.__recordData._attributes.lastname,
+                age: head._objects[i]._internalModel.__recordData._attributes.age,
+                email: head._objects[i]._internalModel.__recordData._attributes.email,
+            }
+            this.data.pushObject(rowInfo);
+        }
 
     }
 });
